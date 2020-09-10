db3c63ad9ccfe4bc328d3de18781fd001aeb0213
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-09-10 23:18:02 +0530

    translation...chanign the sleected values to "g"

---
+++ changiong volvels to g.py
@@ -0,0 +1,16 @@
+def trans(ph):
+    trans= ""
+    for i in ph:
+        if i in "AEIOUaeiou":
+            trans = trans + "g"
+        else:
+            trans = trans + i
+    return trans
+
+ph = input("Enter your word : ")
+
+print(trans(ph))    
+
+
+
+
