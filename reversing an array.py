eeaae4ed1c03011940eab18ded9f8de74937a5cc
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-10-18 14:46:22 +0530

    reverse of an array

---
+++ reversing an array.py
@@ -0,0 +1,18 @@
+# method number one for array reverse
+# def arrayrev(a,i,e):
+#     while i<e:
+#         a[i], a[e] = a[e] , a[i]
+#         i=i+1
+#         e=e-1
+        
+# a=[12,23,434,45,46,57,3,23,23,2,3,4,123,12,3,25,346,25,6,57,656]
+# arrayrev(a,0,(len(a)-1))
+# print(a)
+
+
+# def arry(a):
+#     v=a[::-1]
+#     return v
+# a=[12.3,23,23,4234,2,34,1345,134,5,546,546,43]
+# print(arry(a))
+
