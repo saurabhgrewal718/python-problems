08b3edcbc009202e7e007261ea8b1d53a7548063
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-10-24 14:05:52 +0530

    commiting a new message

--- maximum and minimum of an array.py
+++ maximum and minimum of an array.py
@@ -1,6 +1,24 @@
-a=[12,2,2,34,235,67,43,45,567,65,83,258,74,54,767]
-a.sort()
-lastelement= a[len(a)-1]
-firstelement = a[0]
-print(lastelement,firstelement)
-print(a) 
+# a=[12,2,2,34,235,67,43,45,567,65,83,258,74,54,767]
+# a.sort()
+# lastelement= a[len(a)-1]
+# firstelement = a[0]
+# print(lastelement,firstelement)
+# print(a) 
+
+def arrayfun(a):
+    if(len(a)<1):
+        return "empty array"
+    elif(len(a)==1):
+        return (a[0])
+    elif(len(a)==2):
+        if(a[0]<a[1]):
+            return a[0]
+        else:
+            return a[1]
+    elif(len(a)>3):
+        return a
+        
+
+a=[]
+b= arrayfun(a)
+print(b)
