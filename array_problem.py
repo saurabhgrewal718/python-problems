3ed93756e861474b4f35671db18570bc81f74031
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-01-14 22:06:11 +0530

    solved the array_problem optimallty (leetcode)

---
+++ array_problem.py
@@ -0,0 +1,27 @@
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+nums=[1,1,2,2,4,5,5]
+res = nums[0]
+
+for i in range(1,len(nums)):
+    res=res^nums[i]
+
+print(res)
+
+
+
+
+# def singleNumber(nums:a) :
+#     nums.sort()
+#     for i in range(len(nums)):
+#         if(nums[i]==nums[i+1]):
+#             pass
+#         else:
+#             return nums[i]
+
+
+
+# b= singleNumber(a)
+# print(b)
--- union of two array.py
+++ union of two array.py
@@ -1,14 +1,8 @@
-a=[1,2,3,4,5,6,7,8,9,12,12,21,14]
-b= [12,23,34,54,65,67,78,98,67,56,5,34,232,57]
-u=[]
-def unionarray(a,b):
-    u=list(a)
-    v=u+b
-    v.sort()
-    
+number = int(input("Enter your number! "))
 
-
-
-
-unionarray(a,b)
+if(number>0):
+    number2 = number*5
+    print(number2)
+else:
+    print("Something went wrong")
 
